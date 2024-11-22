de7801eb7e59643ebd1b6837420b54ca42dcf879
Burrellz <burrellz@uwplatt.edu>
2024-11-22 10:03:56 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,41 +2,29 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Brand</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">Homepage</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Official Psyche Website</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Learn More</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Videos</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Credits</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <button data-mdb-button-init class="navbar-toggler ms-auto" type="button" data-mdb-collapse-init
+      data-mdb-target="#navbarToggleExternalContent3" aria-controls="navbarToggleExternalContent3"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+  </div>
+</nav>
+<div class="collapse" id="navbarToggleExternalContent3">
+  <div class="bg-body-tertiary shadow-3 p-4">
+    <button data-mdb-button-init data-mdb-ripple-init
+      class="btn btn-link btn-block border-bottom m-0">Homepage</button>
+    <button data-mdb-button-init data-mdb-ripple-init
+      class="btn btn-link btn-block border-bottom m-0">Official Psyche Page</button>
+    <button data-mdb-button-init data-mdb-ripple-init
+      class="btn btn-link btn-block m-0">Learn More</button>
+           <button data-mdb-button-init data-mdb-ripple-init
+      class="btn btn-link btn-block m-0">Videos</button>
+           <button data-mdb-button-init data-mdb-ripple-init 
+      class="btn btn-link btn-block m-0">Credits</button>
+  </div>
+</div>
   );
 }
 
